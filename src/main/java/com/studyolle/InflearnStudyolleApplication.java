a767d892d7c5eae5d6a8facb1d9c6c74fa0cf160
wilgur513 <wilgur513@naver.com>
2021-10-14 17:58:34 +0900

    first commit

---
+++ src/main/java/com/studyolle/InflearnStudyolleApplication.java
@@ -0,0 +1,13 @@
+package com.studyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnStudyolleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnStudyolleApplication.class, args);
+    }
+
+}
