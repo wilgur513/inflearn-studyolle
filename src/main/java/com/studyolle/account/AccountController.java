9d6922e8adf2eb2ccd2d3af684f3f67bf764418b
wilgur513 <wilgur513@naver.com>
2021-10-14 20:33:20 +0900

    Feat: 수정 AccountController.signUpForm 에서 모델에 signUpForm 객체 저장

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
-    @GetMapping("sign-up")
+    @GetMapping("/sign-up")
     public String signUpForm(Model model) {
-        return "/account/sign-up";
+        model.addAttribute("signUpForm", new SignUpForm());
+        return "account/sign-up";
     }
 }
