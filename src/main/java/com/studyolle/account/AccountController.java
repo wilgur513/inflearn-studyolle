4f0ba420193201b47e4b3143c8ec411fc9aba526
wilgur513 <wilgur513@naver.com>
2021-10-15 15:13:03 +0900

    Feat: 추가 AccountController.signUpForm 회원가입 POST 요청 처리

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -2,7 +2,11 @@ package com.studyolle.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
@@ -10,5 +14,14 @@ public class AccountController {
     public String signUpForm(Model model) {
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if(errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
