e8ecfa81bc19311768971dc48c3e66be53bd8a26
wilgur513 <wilgur513@naver.com>
2021-10-15 15:24:37 +0900

    Feat: 추가 SignUpFormValidator

---
+++ src/main/java/com/studyolle/account/SignUpFormValidator.java
@@ -0,0 +1,30 @@
+package com.studyolle.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SignUpForm form = (SignUpForm) target;
+
+        if(accountRepository.existsByEmail(form.getEmail())) {
+            errors.rejectValue("email", "invalid.email", "중복된 이메일이 존재합니다.");
+        }
+
+        if(accountRepository.existsByNickname(form.getNickname())) {
+            errors.rejectValue("nickname", "invalid.nickname", "중복된 닉네임이 존재합니다.");
+        }
+    }
+}
