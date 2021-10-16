115a40e40a1af5b225e1163bf056292591507a06
wilgur513 <wilgur513@naver.com>
2021-10-16 16:43:52 +0900

    Feat: 추가 회원가입 및 이메일 인증 후 자동 로그인

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -36,7 +36,8 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        accountService.processNewAccount(signUpForm);
+        Account account = accountService.processNewAccount(signUpForm);
+        accountService.login(account);
         return "redirect:/";
     }
 
@@ -51,6 +52,7 @@ public class AccountController {
         }
 
         account.completeSignUp();
+        accountService.login(account);
         model.addAttribute("numberOfUser", accountRepository.count());
         model.addAttribute("nickname", account.getNickname());
         return view;
