3feee0d6979a1a4c0e58452ab73f056a816e2412
wilgur513 <wilgur513@naver.com>
2021-11-02 17:16:34 +0900

    Feat: 추가 CurrentUser 어노테이션

---
+++ src/main/java/com/studyolle/account/CurrentUser.java
@@ -0,0 +1,11 @@
+package com.studyolle.account;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : account")
+public @interface CurrentUser {
+}
