a24ee3fd08c745b35204a527ac39859ce69018bc
wilgur513 <wilgur513@naver.com>
2021-11-23 18:07:28 +0900

    Test: 추가 닉네임을 이용한 로그인 테스트
    ParameterizedTest로 작성

--- src/test/java/com/studyolle/main/MainControllerTest.java
+++ src/test/java/com/studyolle/main/MainControllerTest.java
@@ -9,8 +9,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.studyolle.account.AccountService;
 import com.studyolle.account.SignUpForm;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,25 +31,27 @@ class MainControllerTest {
     @Autowired
     AccountService accountService;
 
-    @Test
-    @DisplayName("이메일을 이용한 로그인")
-    void loginWithEmail() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
         SignUpForm form = new SignUpForm();
         form.setNickname("username");
         form.setEmail("email@email.com");
         form.setPassword("password");
         accountService.processNewAccount(form);
+    }
 
+    @ParameterizedTest(name="아이디 : {0}")
+    @ValueSource(strings={"email@email.com", "username"})
+    @DisplayName("이메일 또는 닉네임 로그인 테스트")
+    void loginWithEmail(String emailOrNickname) throws Exception {
         mockMvc.perform(post("/login")
-            .param("username", "email@email.com")
+            .param("username", emailOrNickname)
             .param("password", "password")
             .with(csrf())
         )
             .andDo(print())
             .andExpect(status().is3xxRedirection())
             .andExpect(redirectedUrl("/"))
-            .andExpect(authenticated().withUsername("username"))
-        ;
-
+            .andExpect(authenticated().withUsername("username"));
     }
 }
