8e36130d6d6f403996a660b4fbbc5345ae972472
wilgur513 <wilgur513@naver.com>
2021-11-23 18:29:48 +0900

    Refactor: import문 정리

--- src/main/java/com/studyolle/main/MainController.java
+++ src/main/java/com/studyolle/main/MainController.java
@@ -5,7 +5,6 @@ import com.studyolle.domain.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
